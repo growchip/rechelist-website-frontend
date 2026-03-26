63e3a79a59240b44db894fc99531cf8c6e62cbee
Abhinav Rawat <abhinav63381@gmail.com>
2026-03-26 11:04:06 +0530

    Pixel code added

--- app/layout.tsx
+++ app/layout.tsx
@@ -67,6 +67,7 @@ export default async function RootLayout({
         async
         src="https://www.googletagmanager.com/gtag/js?id=G-R3ZEVTRB7D"
       ></script>
+      
       <script
         dangerouslySetInnerHTML={{
           __html: `
@@ -94,7 +95,24 @@ export default async function RootLayout({
         }}
       />
 
-
+      {/* Meta Pixel */}
+      <script
+        dangerouslySetInnerHTML={{
+          __html: `
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1373798054504152');
+            fbq('track', 'PageView');
+          `,
+        }}
+      />
+      
       </head>
       <body>
         <div id="backdrop-root"></div>
