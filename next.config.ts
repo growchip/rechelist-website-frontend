31419421ee090f7fd78731807cf56f0b31797613
Abhinav Rawat <abhinav63381@gmail.com>
2026-03-26 12:23:26 +0530

    Redirection

--- next.config.ts
+++ next.config.ts
@@ -10,6 +10,21 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: 'rechelistpharma.com',
+          },
+        ],
+        destination: 'https://www.rechelistpharma.com/:path*',
+        permanent: true, // This triggers the 301 status code
+      },
+    ]
+  },
 };
 
 export default nextConfig;
